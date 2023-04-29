355cb147649dcd87e8dc82431caf52e16ef5b974
Daksh Bhatia <daksh.bhatia@bobble.ai>
2023-04-30 01:56:25 +0530

    Added check in speed clicker

--- lib/SpeedClicker/home.dart
+++ lib/SpeedClicker/home.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:cherry_toast/cherry_toast.dart';
+import 'package:cherry_toast/resources/arrays.dart';
 
 class SpeedClicker extends StatefulWidget {
   const SpeedClicker({super.key});
-
 
   @override
   State<SpeedClicker> createState() => _SpeedClickerState();
 }
 
 class _SpeedClickerState extends State<SpeedClicker> {
-
   var startButtonVisible = true;
   var lightsVisible = false;
 
@@ -26,8 +26,6 @@ class _SpeedClickerState extends State<SpeedClicker> {
   var score = '0';
 
   Timer? gameTimer, redLightTimer, yellowLightTimer, greenLightTimer;
-
-
 
   _startGame() {
     setState(() {
@@ -46,7 +44,8 @@ class _SpeedClickerState extends State<SpeedClicker> {
     gameTimer = Timer.periodic(const Duration(milliseconds: 1), (timer) {
       setState(() {
         print("stopwatchText: $stopwatchText");
-        stopwatchText = (double.parse(stopwatchText) + 0.001).toStringAsFixed(3);
+        stopwatchText =
+            (double.parse(stopwatchText) + 0.001).toStringAsFixed(3);
       });
     });
   }
@@ -70,112 +69,137 @@ class _SpeedClickerState extends State<SpeedClicker> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                startButtonVisible
-                   ? Column(
-                     children: [
-                       Text(
-                 'Speed Clicker',
-                 style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                       const SizedBox(height: 20),
-                  // Score
-                  Text(
-                    'Score: $score',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                       const SizedBox(height: 20),
-                       ElevatedButton(
-                         style: ElevatedButton.styleFrom(
-                           foregroundColor: Colors.white, backgroundColor: Colors.black,
-                         ),
-                         onPressed: () {
-    _startGame();
-                         },
-                         child: const Text('Start'),
-                       ),
-                     ],
-                   )
-                    : Container(),
-                lightsVisible ?
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // Red light, Yellow light, Green light
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            color: redLightVisible ? Colors.red : Colors.white10,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            color: yellowLightVisible ? Colors.yellow : Colors.white10,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            color: greenLightVisible ? Colors.green : Colors.white10,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ],
-                    ),
-                    // Timer
-                    Text(
-                      stopwatchText,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                    // Tap area
-                    GestureDetector(
-                      onTap: () {
-                        if (tapEnabled) {
-                          setState(() {
-                            lightsVisible = false;
-                            startButtonVisible = true;
-                            score = stopwatchText;
-                            stopwatchText = '0.000';
-                            gameTimer?.cancel();
-                            tapEnabled = false;
-                          });
-                        }
-                      },
-                      child: Container(
-                        color: Colors.grey,
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        height: MediaQuery.of(context).size.height * 0.25,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            startButtonVisible
+                ? Column(
+                    children: [
+                      Text(
+                        'Speed Clicker',
+                        style: Theme.of(context).textTheme.headlineMedium,
                       ),
-                    ),
-                  ],
-                )
-                    : Container(),
-
-              ]
-          ),
-        )
-    );
-
-
+                      const SizedBox(height: 20),
+                      // Score
+                      Text(
+                        'Score: $score',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                        onPressed: () {
+                          _startGame();
+                        },
+                        child: const Text('Start'),
+                      ),
+                    ],
+                  )
+                : Container(),
+            lightsVisible
+                ? Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Red light, Yellow light, Green light
+                          Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color:
+                                  redLightVisible ? Colors.red : Colors.white10,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: yellowLightVisible
+                                  ? Colors.yellow
+                                  : Colors.white10,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.all(10),
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: greenLightVisible
+                                  ? Colors.green
+                                  : Colors.white10,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Timer
+                      Text(
+                        stopwatchText,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1),
+                      // Tap area
+                      GestureDetector(
+                        onTap: () {
+                          if (tapEnabled) {
+                            setState(() {
+                              lightsVisible = false;
+                              startButtonVisible = true;
+                              score = stopwatchText;
+                              stopwatchText = '0.000';
+                              tapEnabled = false;
+                              gameTimer?.cancel();
+                              redLightTimer?.cancel();
+                              yellowLightTimer?.cancel();
+                              greenLightTimer?.cancel();
+                            });
+                          } else {
+                            CherryToast.warning(
+                              title: const Text('Wait Wait Wait'),
+                              displayTitle: false,
+                              description: const Text(
+                                  'You tapped too early! Wait for green light.'),
+                              animationType: AnimationType.fromTop,
+                              animationDuration:
+                                  const Duration(milliseconds: 500),
+                            ).show(context);
+                            setState(() {
+                              lightsVisible = false;
+                              startButtonVisible = true;
+                              score = "0";
+                              stopwatchText = '0.000';
+                              tapEnabled = false;
+                              gameTimer?.cancel();
+                              redLightTimer?.cancel();
+                              yellowLightTimer?.cancel();
+                              greenLightTimer?.cancel();
+                            });
+                            print('Tap disabled');
+                          }
+                        },
+                        child: Container(
+                          color: Colors.grey,
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          height: MediaQuery.of(context).size.height * 0.25,
+                        ),
+                      ),
+                    ],
+                  )
+                : Container(),
+          ]),
+    ));
   }
-  
 }
